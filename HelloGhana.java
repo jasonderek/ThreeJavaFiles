d876f33592e2c4fcd82a23c7df4c82bad35db15d
jasonderek <derekbossman@gmail.com>
2013-08-24 15:43:47 +0000

    Update HelloGhana.java
    from me again :)

--- HelloGhana.java
+++ HelloGhana.java
@@ -2,5 +2,6 @@ class HelloGhana extends Parent implements Interface {
 	public static void main(String[] args) {
 		System.out.print("Hello World!");
 		System.out.print("Hello World!");
+		System.out.print("Hello World!");
 	}
 }
